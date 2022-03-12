1d07b4ce3b98d6dc8adc3a9707975312e252e2a2
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-03-12 16:37:45 +0000

    no internet error

---
+++ app/src/main/java/es/easygas/ErrorActivity.java
@@ -0,0 +1,16 @@
+package es.easygas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+
+
+    }
+}
--- app/src/main/java/es/easygas/MainActivity.java
+++ app/src/main/java/es/easygas/MainActivity.java
@@ -2,7 +2,10 @@ package es.easygas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,10 +18,25 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(MainActivity.this, WebActivity.class));
+                if (isConnected() == true){
+                    startActivity(new Intent(MainActivity.this, WebActivity.class));
+                }else{
+                    startActivity(new Intent(MainActivity.this, ErrorActivity.class));
+                }
                 finish();
             }
         }, 2000);
 
+    }
+
+    private boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        boolean activo = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            activo = true;
+        }
+        return activo;
     }
 }
