4bc7e1713972c2327b10a7760990f1a605d3a3a6
Pablo <47283527+PabloSR06@users.noreply.github.com>
2022-03-12 00:54:05 +0000

    splashscren

--- app/src/main/java/es/easygas/MainActivity.java
+++ app/src/main/java/es/easygas/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Intent intent= new Intent (this, MainActivity.class);
-        startActivity(intent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, WebActivity.class));
+                finish();
+            }
+        }, 2000);
+
     }
 }
